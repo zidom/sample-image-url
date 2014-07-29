d4835a929142578b77ddd35852c8f5d8d58c9db5
zidom <zidom@126.com>
2014-07-29 18:24:13 +0800

    修改web faclet

--- src/main/java/org/zidom/image/WebMvcDispatcherServletInitializer.java
+++ src/main/java/org/zidom/image/WebMvcDispatcherServletInitializer.java
@@ -1,23 +1,48 @@
 package org.zidom.image;
 
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+import org.zidom.image.config.WebMvcConfig;
 
+/**
+ * Spring mvc 配置
+ * 
+ * @author zidom
+ *
+ */
 public class WebMvcDispatcherServletInitializer extends
 		AbstractAnnotationConfigDispatcherServletInitializer {
 
+	/**
+	 * 
+	 * root 容器配置
+	 * 
+	 * @see org.springframework.web.servlet.support.
+	 *      AbstractAnnotationConfigDispatcherServletInitializer
+	 *      #getRootConfigClasses()
+	 */
 	@Override
 	protected Class<?>[] getRootConfigClasses() {
-		return null;
+		return new Class[] { WebMvcConfig.class };
 	}
 
+	/**
+	 * 使用默认
+	 * 
+	 * @see org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer#getServletConfigClasses()
+	 */
 	@Override
 	protected Class<?>[] getServletConfigClasses() {
 		return null;
 	}
 
+	/**
+	 * 配置DispaterServlet路径
+	 * 
+	 * @see org.springframework.web.servlet.support.AbstractDispatcherServletInitializer#getServletMappings()
+	 */
 	@Override
 	protected String[] getServletMappings() {
-		return null;
+		return new String[] { "/" };
 	}
 
 }
